228091f82a53ffc318552b34f770e0053ac0858a
Kapil Rajput <rajputkapil436@gmail.com>
2025-01-14 19:42:37 +0530

    initial

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import './index.css';
+import App from './App';
+
+import { ContextProvider } from './context/ContextProvider';
+
+ReactDom.render(
+    <ContextProvider><App /> </ContextProvider>, document.getElementById('root'));
